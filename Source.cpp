15bc927f792de2fa6305521dc70dd6b008a3158d
Hao Yu <yuhayo@mail.ru>
2022-09-28 12:25:24 +0300

    true

--- Source.cpp
+++ Source.cpp
@@ -5,26 +5,70 @@ using namespace std;
 
 int main()
 {
-	int l = 0, m = 0, r = -1, sum = 0, loc = 0;
-	const int N = 10;
+	int l = 0,m=0, r = -1, sum = 0, loc = 0;
+	const int N = 1000;
 	int a[N];
-	int b[N]
+
 
 
 	srand(time(0));
 	for (int i = 0; i < N; i++)
 	{
 		a[i] = rand() % 1000 - rand() % 1000;
-		cout << a[i] << " ";
+		cout << a[i]<<" ";
 	}
 
-	b[0] = 0;
-	for (int i = 1; i < N; i++)
+
+
+	while (r<N)
 	{
-		b[i] = a[i] + b[i - 1];
+
+
+
+		while (loc + a[r+1]>loc)
+		{
+			loc += a[r + 1];
+			r++;
+		}
+		if (loc > sum)
+		{
+			sum = loc;
+			cout << '\n' << l << " ==> " << r;
+		}
+		r++;
+		while (a[r] < 0)
+		{
+			r++;
+		}
+
+
+		m = r;
+		loc = a[r];
+		while (loc + a[r + 1] > loc)
+		{
+			loc += a[r + 1];
+			r++;
+		}
+		if (loc > sum)
+		{
+			sum = loc;
+			cout << '\n' << m << " ==> " << r;
+		}
+
+
+		loc = 0;
+		for (int i = l; i < r + 1; i++)
+		{
+			loc += a[i];
+		}
+		if (loc > sum)
+		{
+			sum = loc;
+			cout << '\n' << l << " ==> " << r;
+		}
+		l = m;
+		r = l;
+		loc = 0;
 	}
-	for (int i = 0; i <N ; i++)
-	{
-		cout << b[i] << ' ';
-	}
+	cout << '\n' << sum;
 }
